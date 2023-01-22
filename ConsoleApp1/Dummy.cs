a38990fb892161b8ca50a33d1e61267275ae48fb
Mahesh Pattar <maheshgld777@gmail.com>
2023-01-22 22:49:16 +0530

    Verify Palindrome

--- ConsoleApp1/Dummy.cs
+++ ConsoleApp1/Dummy.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] arg)
         {
-            FindPalindrome("GADAG");
+            FindPalindrome("GADACAG");
             Console.ReadLine();
         }
 
