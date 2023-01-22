74b4e5cd871a2daae79e29aeaea15bd4a2fd2b1a
Mahesh <Mahesh@DESKTOP-9U8FRGU>
2023-01-22 22:45:27 +0530

    Add project files.

---
+++ ConsoleApp1/Dummy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Dummy
+    {
+        static void Main(string[] arg)
+        {
+            FindPalindrome("GADAG");
+            Console.ReadLine();
+        }
+
+        static void FindPalindrome(string input)
+        {
+            for (int i = 0, j = input.Length - 1; i < input.Length - 1; i++, j--)
+            {
+                if (input[i] == input[j])
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Given input {input} is not palindrome");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Given input {input} is palindrome");
+        }
+    }
+}
